3a6cacdd90d7867aa28e2233e4fbb4bcaa8afc10
CHBChan <four@umich.edu>
2024-02-01 14:47:24 -0500

    Update

--- src/app/api/fetchCards/route.ts
+++ src/app/api/fetchCards/route.ts
@@ -1,7 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 import axios from "axios";
 
+export const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export async function GET(req: NextRequest, res: NextResponse) {
+    
     try {
         const response = await axios.get('https://api.gatcg.com/cards/search?type=CHAMPION');
         const data : any = response.data;
@@ -10,6 +17,8 @@ export async function GET(req: NextRequest, res: NextResponse) {
             message: "Cards retrieved successfully",
             succes: true,
             data: data
+        },{
+            headers: corsHeaders,
         });
     } catch (error : any) {
         return NextResponse.json({error: error.message}, {status: 444});
